db34b7a9474a918a796278fda7571968d50fd24f
amitkushwah2025 <amit.kushwah2025@glbajajgroup.org>
2026-06-16 12:35:00 -0700

    Day 3 Assignment

---
+++ C-Programming-30Day/Day-3/Q9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+printf("Enter the number");
+scanf("%d",&n);
+
+for(i=2;i<n;i++)
+{
+    if(n%i==0)
+{
+    printf("Not Prime");
+    return 0;
+}
+}
+printf("Prime");
+
+return 0;
+
+}
