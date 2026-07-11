4275446537681b66aef5540c9ebe4e54bfb8df1c
amitkushwah2025 <amit.kushwah2025@glbajajgroup.org>
2026-07-11 14:10:07 -0700

    Day 13 Assignment

---
+++ C-Programming-30Day/Day-13/Q52.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10], i;
+    int even = 0, odd = 0;
+
+    printf("Enter 10 elements:\n");
+
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < 10; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+    printf("Odd elements = %d\n", odd);
+
+    return 0;
+}
