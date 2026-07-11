9cd03a795d89b7129c7e7bc9e36ac88f871564b1
amitkushwah2025 <amit.kushwah2025@glbajajgroup.org>
2026-07-11 15:10:04 -0700

    Day 22 Assignment

---
+++ C-Programming-30Day/Day-22/Q85.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, len, palindrome = 1;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    len = strlen(str);
+
+    for(i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - i - 1]) {
+            palindrome = 0;
+            break;
+        }
+    }
+
+    if(palindrome)
+        printf("String is Palindrome.\n");
+    else
+        printf("String is Not Palindrome.\n");
+
+    return 0;
+}
